5d4e999f476898cfcd2a7efa47bfb0cb49b55e02
davemar42 <86600161+davemar42@users.noreply.github.com>
2021-06-28 12:01:38 +0200

    Add files via upload

---
+++ 02.js
@@ -0,0 +1,16 @@
+
+var objeto={
+    nombre: "pablo",
+    apellido: "perez",
+    telefono:"6262626"
+}
+var selector="telefono";
+if(selector=="nombre"){
+    console.log(objeto.nombre)
+}else if (selector=="apellido"){
+    console.log(objeto.apellido)
+
+}else if(selector=="telefono"){
+    console.log(objeto.telefono)
+}
+
